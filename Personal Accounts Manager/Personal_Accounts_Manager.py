1e1394404f2ea8485bc2b7560bb95608586ece54
Mostafa Alakdawi <mostafa.alakdawi@outlook.com>
2020-09-06 06:46:49 +0200

    Adding the following functions - writeToOutputFile(): To write the contents of the Accounts dictionary to an external file - readInputFile(): To read info of User accounts from an external file and save it to   Accounts dictionary

--- Personal Accounts Manager/Personal_Accounts_Manager.py
+++ Personal Accounts Manager/Personal_Accounts_Manager.py
@@ -1,3 +1,4 @@
+import os
 import random
 from datetime import datetime
 
@@ -5,7 +6,7 @@ from datetime import datetime
 Accounts = {}
 
 #A list to contain all the characters used for generating
-#the random password
+#the random password (91 character)
 Characters = ['Q','W','E','R','T','Y','U','I','O','P',
               'A','S','D','F','G','H','J','K','L','Z',
               'X','C','V','B','N','M','1','2','3','4',
@@ -17,6 +18,12 @@ Characters = ['Q','W','E','R','T','Y','U','I','O','P',
               'm',',','.','/','{','}',':','"','|','<',
               '>','?']
 
+#Name of the input/output file
+inputFileName = "testOutputFile.txt"
+
+#Updated name of the input/output file
+inputFileNameUpdated = "testOutputFileOLD.txt"
+
 #A class for the Account object
 class Account:
 
@@ -24,6 +31,14 @@ class Account:
         self.Organisation = organisation
         self.Email = email
         self.Password = password
+
+#To print the dictionary of Accounts for debugging purposes
+def printAccounts():
+    for key in Accounts.keys():
+        print("Organisaion: " + Accounts[key].Organisation)
+        print("e-mail: ",Accounts[key].Email)
+        print("password: ",Accounts[key].Password)
+        print("\n")
 
 #To view account info if the account is present in the Accounts dictionary
 def viewAccount(Organisation):
@@ -57,13 +72,49 @@ def accountsManager():
         account = Account(Organisation, email, password)
         Accounts[account.Organisation] = account
 
+#To write the contents of the Accounts dictionary to an external file
+def writeToOutputFile():
+    inputFile = open(inputFileName,"w+")
+    accountsData = ""
+    for key in Accounts.keys():
+        accountsData += "Organsation: "+ key + "\n"
+        accountsData += "E-mail: " + Accounts[key].Email + "\n"
+        accountsData += "Password: " + Accounts[key].Password + "\n"
+        accountsData += "\n\n"
+    inputFile.write(accountsData)
+    inputFile.close()
+    os.remove("./" + inputFileNameUpdated)
+
+#To read info of User accounts from an external file and save it to
+# Accounts dictionary
+def readInputFile():
+
+    if( not os.path.isfile("./" + inputFileName) ):
+        print("No Previous Data Found")
+        return
+
+    inputFile = open(inputFileName,"r")
+    accountsData = inputFile.readlines()
+    inputFile.close()
+    os.rename("./" + inputFileName, "./" + inputFileNameUpdated)
+
+    for lineIndex in range(0,len(accountsData),5):
+
+        organisation = accountsData[lineIndex].replace("Organsation: ","").replace("\n","")
+        email = accountsData[lineIndex+1].replace("E-mail: ","").replace("\n","")
+        password = accountsData[lineIndex+2].replace("Password: ","").replace("\n","")
+
+        account = Account(organisation, email, password)
+        Accounts[organisation] = account
 
 #Start of Program
 Input = input("Enter anything but 0\n")
+readInputFile()
+
+#printAccounts()
 
 while (Input != "0"):
     accountsManager()
     Input = input("Enter anything but 0\n")
 
-
-
+writeToOutputFile()
