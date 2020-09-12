f947b054c14c66cc18f9f21129c782330c7d395e
Mostafa Alakdawi <mostafa.alakdawi@outlook.com>
2020-09-12 20:30:39 +0200

    Updating the GUI_Interface with the following: - Adding a function call to readInputFile() in function GUI_Start() - Updating some of the labels (Organisation, E-mail, Password) - Movig global variables to the top of the file - Updating the text getter function for Organisation text field

--- Personal Accounts Manager/GUI_Interface.py
+++ Personal Accounts Manager/GUI_Interface.py
@@ -1,6 +1,14 @@
 from tkinter import *
 from Personal_Accounts_Manager import *
 
+# Creating the GUI elements
+master = Tk()
+
+# Creating the GUI text fields
+organisationText = Text(master, height=1, width=30)
+emailText = Text(master, height=1, width=30)
+passwordText = Text(master, height=1, width=30)
+
 #To view account info in the specified text fieds
 def viewAccountInfoText(accountInfo):
     eraseText()
@@ -13,7 +21,7 @@ def viewAccountInfoText(accountInfo):
 def accountManagerStart():
     emailText.delete('1.0', END)
     passwordText.delete('1.0', END)
-    organisation = organisationText.get("1.0",END)
+    organisation = organisationText.get("1.0",'end-1c')
     viewAccountInfoText(mainFunction(organisation))
 
 #To erase account info from the specified text fieds
@@ -36,9 +44,9 @@ def GUI_Init():
     master.geometry("1000x400")
 
     # Creating the GUI labels
-    Label(master, width = 20, text='Organisation: ', font=("Times New Roman", 20, "bold")).grid(row=0)
-    Label(master, width = 20, text='E-mail: ', font=("Times New Roman", 20, "bold")).grid(row=1)
-    Label(master, width = 20, text='Password: ', font=("Times New Roman", 20, "bold")).grid(row=2)
+    Label(master, width = 15, text='Organisation: ', font=("Times New Roman", 20, "bold")).grid(row=0)
+    Label(master, width = 15, text='E-mail: ', font=("Times New Roman", 20, "bold")).grid(row=1)
+    Label(master, width = 15, text='Password: ', font=("Times New Roman", 20, "bold")).grid(row=2)
     Label(master, width = 20, text='Password Options', font=("Times New Roman", 20, "bold")).grid(row=0, column=2)
 
     # Configuring the font for the GUI text fields
@@ -66,6 +74,11 @@ def GUI_Init():
 
 #To start the execution of the GUI_Interface module
 def GUI_Start():
+
+    #Read the already existing info of the accounts
+    #from the input text file
+    readInputFile()
+
     #Initialization of the GUI elements
     GUI_Init()
 
@@ -75,14 +88,6 @@ def GUI_Start():
     #Writing the info of the Accounts dictionary to an external text file
     #to save this info for future use
     writeToOutputFile()
-
-# Creating the GUI elements
-master = Tk()
-
-# Creating the GUI text fields
-organisationText = Text(master, height=1, width=15)
-emailText = Text(master, height=1, width=15)
-passwordText = Text(master, height=1, width=15)
 
 #Starting the execution of the GUI
 GUI_Start()
